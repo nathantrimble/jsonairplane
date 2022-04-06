383d3ba038048dab4cc54f04f6bf45a7f1a21c5b
Nate <nathantrimble1@gmail.com>
2022-04-06 09:52:45 -0700

    day 4 cringe

--- webapp.py
+++ webapp.py
@@ -11,23 +11,39 @@ def render_main():
 
 @app.route("/airport")
 def render_airportdata():
+
     return render_template('airportdata.html')
+
+
+@app.route('/airportresponse')
+def render_aresponse():
+    with open('airlines.json') as airlines:
+        adata = json.load(airlines)
+
+    chosenap = request.args['aps']
+    choseyear = request.args['years']
+    chosemonth = request.args['months']
+    airlAmount = 0
+    airlNames = ""
+    totFlights = 0
+    for a in adata:
+        if a["Airport"]["Code"] == chosenap and a['Time']['Year'] == choseyear and a['Time']['Month'] == chosemonth:
+            airlAmount = a['Statistics']['Carriers']["Total"]
+            airlNames = a['Statistics']['Carriers']["Names"]
+            totFlight = a['Statistics']["Flights"]['Total']
+        return airlNames
+        return airlAmount
+        return totFlight
+    return render_aresponse('airportresponse.html')
+
+
+
+
 
 @app.route("/delay")
 def render_delaydata():
     return render_template('delaydata.html')
 
-def getairportdata():
-    with open('airlines.json') as airlines:
-        adata = json.load(airlines)
-
-    chosenap = request.args['aps']
-    airlAmount = 0
-    airlNames = ""
-    for a in adata:
-        if a["Name"] == chosenap:
-            airlAmount = a["Total"]
-            airlNames = a["Names"]
 
 
 if __name__=="__main__":
